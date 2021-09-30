fb5c27786636dadec8a47b6e9bd86861d53952a7
Igor Sokolenko <i.sokolenko@iml.ru>
2021-09-30 18:25:33 +0300

    chore: graceful shutdown mv to srv package

--- cmd/main.go
+++ cmd/main.go
@@ -1,40 +1,29 @@
 package main
 
 import (
-	"context"
 	"os"
-	"os/signal"
-	"syscall"
 	"time"
 
 	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/mongo"
 
 	"github.com/Yosh11/url-short-test/internal/err"
 	"github.com/Yosh11/url-short-test/internal/srv"
 )
 
 func main() {
+	// Init env`s
 	e := godotenv.Load()
 	err.CheckFatal(e,"fail with env`s")
+	// Init custom logger
 	err.InitLogrus()
 
+	// Init repository (MondoDB)
 	db := srv.InitMongo()
+
+	// New server
 	s := new(srv.Server)
 	go err.CheckError(s.Run(os.Getenv("PORT_API"), nil))
 
-	graceful(s, db, 10*time.Second)
-}
-
-func graceful(s *srv.Server, db *mongo.Client, timeout time.Duration) {
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
-	<-quit
-
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	defer cancel()
-
-	err.CheckError(db.Disconnect(ctx), "Database forced to shutdown")
-	err.CheckFatal(s.Shutdown(ctx), "Server forced to shutdown")
-	err.Info("Server exiting")
+	// Try shutdown app
+	srv.GracefulShutdown(s, db, 10*time.Second)
 }
---
+++ internal/srv/graceful.go
@@ -0,0 +1,26 @@
+package srv
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+
+	"github.com/Yosh11/url-short-test/internal/err"
+)
+
+func GracefulShutdown(s *Server, db *mongo.Client, timeout time.Duration) {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	err.CheckError(db.Disconnect(ctx), "Database forced to shutdown")
+	err.CheckFatal(s.Shutdown(ctx), "Server forced to shutdown")
+	err.Info("Server exiting")
+}
